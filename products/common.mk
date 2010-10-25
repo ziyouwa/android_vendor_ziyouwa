3dddefc6a00460cbf23b2f1333bae2e9959ab429
YangJie <yjsss75@gmail.com>
2010-10-25 11:09:16 +0800

    sync

--- products/common.mk
+++ products/common.mk
@@ -63,7 +63,7 @@ include frameworks/base/data/sounds/AudioPackage4.mk
 
 PRODUCT_COPY_FILES += \
     vendor/ziyouwa/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/ziyouwa/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml \
+    vendor/ziyouwa/prebuilt/common/bin/verify_cache_partition_size.sh:system/bin/verify_cache_partition_size.sh \
     vendor/ziyouwa/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf \
     vendor/ziyouwa/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
     vendor/ziyouwa/prebuilt/common/etc/terminfo/l/linux:system/etc/terminfo/l/linux \
